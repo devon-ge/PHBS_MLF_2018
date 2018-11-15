897e0c3af102371519ee3aac5d8f013fa0ada2ca
Devon Ge <1701213756@sz.pku.edu.cn>
2018-11-15 16:47:54 +0800

    Delete unused codes

--- test/compress_func.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-
-def compress_image(img):
-    """ compress a picture with `ratio' """
-    com_img = Image.open(img)
-    size = 256, 256
-    com_img.thumbnail(size)
-    com_img.save(f'com_{img}', 'png')
